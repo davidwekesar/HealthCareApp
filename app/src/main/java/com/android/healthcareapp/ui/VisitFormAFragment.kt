6d5620e828377db5ff960aac5bc74b73c44d25d9
davidwekesar <davidwekesar@gmail.com>
2021-11-22 22:38:23 +0300

    add patient visit form A fragment

---
+++ app/src/main/java/com/android/healthcareapp/ui/VisitFormAFragment.kt
@@ -0,0 +1,28 @@
+package com.android.healthcareapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.healthcareapp.databinding.FragmentVisitFormABinding
+
+class VisitFormAFragment: Fragment() {
+
+    private var _binding: FragmentVisitFormABinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentVisitFormABinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
